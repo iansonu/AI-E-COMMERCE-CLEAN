7bd1d37d5206087dd67ac30d01f5070b5c9fc0e1
iansonu <sonunishad2100@gmail.com>
2026-02-24 14:07:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://ai-e-commerce-clean-frontend.onrender.com" , "http://localhost:5174"],
+ origin:["https://ai-e-commerce-clean-frontend.onrender.com" , "https://ai-e-commerce-clean-admin.onrender.com"],
  credentials:true
 }))
 
